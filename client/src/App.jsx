d9c14b0b873bbb2afcf1b39fa280de5c11d5dee7
Caio Silva <caiossantosp5560@gmail.com>
2024-04-09 14:41:58 -0300

    feat: create pages and routes with react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import About from './pages/About';
+import Projects from './pages/Projects'
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
 const App = () => {
   return (
     <>
-      <div className='text-2xl'>
-        Ola
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/about' element={<About />}/>
+          <Route path='/sign-up' element={<SignUp />}/>
+          <Route path='/sign-in' element={<SignIn />}/>
+          <Route path='/projects' element={<Projects />}/>
+          <Route path='/dashboard' element={<Dashboard />}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
