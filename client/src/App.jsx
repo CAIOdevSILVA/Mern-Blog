6af1352156773f8c77d6341471dfd2090e0ae235
Caio Silva <caiossantosp5560@gmail.com>
2024-04-10 15:05:23 -0300

    feat: create a header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,13 @@ import About from './pages/About';
 import Projects from './pages/Projects'
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path='/' element={<Home />}/>
           <Route path='/about' element={<About />}/>
