4d0ac11e2b77e890de805539bf721f44570064d4
Caio Silva <caiossantosp5560@gmail.com>
2024-04-11 15:36:43 -0300

    feat: add MongoDB connection with the server

--- api/index.js
+++ api/index.js
@@ -1,5 +1,10 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config';
 
+mongoose.connect(process.env.MONGODB).then(() => {
+  console.log('MongoDB is connected');
+});
 const app = express();
 
 app.listen('3000', () => {
