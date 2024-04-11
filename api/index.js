06d0bb08c441ef2f818c0a1f80a32d624d0e204e
Caio Silva <caiossantosp5560@gmail.com>
2024-04-11 16:21:53 -0300

    feat: add test api route

--- api/index.js
+++ api/index.js
@@ -1,11 +1,15 @@
-import express from 'express';
+import express, { json } from 'express';
 import mongoose from 'mongoose';
+import userRoutes from './routes/user.route.js';
+
 import 'dotenv/config';
 
 mongoose.connect(process.env.MONGODB).then(() => {
   console.log('MongoDB is connected');
 });
 const app = express();
+
+app.use('/api/user', userRoutes);
 
 app.listen('3000', () => {
   console.log('Server is running on port 3000!')
---
+++ api/routes/user.route.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { test } from '../controllers/user.controller.js';
+
+const router = express.Router();
+
+router.get('/test', (req, res) => {
+  test(req, res);
+});
+
+export default router;
+
+
