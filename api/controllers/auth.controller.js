2ca3f3bb26650a122a1fcf16f36fc9118edeb458
Caio Silva <caiossantosp5560@gmail.com>
2024-04-22 15:00:13 -0300

    feat: create sign-in api route, controller and route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from '../models/User.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
 
 export const signUp = async (req, res, next) => {
   const { username, email, password} = req.body;
@@ -20,6 +22,37 @@ export const signUp = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json({ message: 'Signup seccessfuly' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if(!email || !password || email === '' || password === '') {
+    next(errorHandler(400, 'All fields are required'));
+  };
+
+  try {
+    const validUser = await User.findOne({ email });
+    if(!validUser){
+      return next(errorHandler(400, 'Invalid Credentials'));
+    };
+
+    const validPassword = await bcryptjs.compareSync(password, validUser.password);
+    if(!validPassword) {
+      return next(400, 'Invalid Credentials');
+    };
+
+    const token = jwt.sign(
+      { id: validUser._id },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: '1d' }
+    );
+
+    res.status(200).cookie('access_token', token, { httpOnly: true }).json('Login Successful')
+
   } catch (error) {
     next(error);
   }
