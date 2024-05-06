18d4f351f37ce84507bb1fca987b76dae6787e79
Caio Silva <caiossantosp5560@gmail.com>
2024-05-06 15:55:05 -0300

    feat: add delete api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { test, updateUser } from '../controllers/user.controller.js';
+import { deleteUser, test, updateUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -9,6 +9,7 @@ router.get('/test', (req, res) => {
 });
 
 router.put('/update/:userId', verifyToken, updateUser);
+router.put('/delete/:userId', verifyToken, deleteUser);
 
 export default router;
 
