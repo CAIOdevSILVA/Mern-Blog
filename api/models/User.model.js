bc8d13f0f64ef1a87c02df07fc0b36aae5b507ef
Caio Silva <caiossantosp5560@gmail.com>
2024-05-13 14:44:12 -0300

    feat: add isAdmin functionality to the user

--- api/models/User.model.js
+++ api/models/User.model.js
@@ -18,6 +18,10 @@ const userSchema = new mongoose.Schema({
   profilePicture: {
     type: String,
     default: 'https://th.bing.com/th/id/OIP.1LRUIB2OXVePxD5hQm4fqwHaHa?rs=1&pid=ImgDetMain'
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false
   }
 },
   {
