7b5b7874e81f01bf3d1a16c3e3fa78d5bc623e30
Caio Silva <caiossantosp5560@gmail.com>
2024-05-23 17:27:35 -0300

    feat: add updatePost route and functionality in the backend

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { create, deletePosts, getPosts } from '../controllers/post.controller.js';
+import { create, deletePosts, getPosts, updatePost } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, create);
 router.get('/get-posts', getPosts);
 router.delete('/delete-post/:postId/:userId', verifyToken, deletePosts);
+router.put('/update-post/:postId/:userId', verifyToken, updatePost);
 
 export default router;
