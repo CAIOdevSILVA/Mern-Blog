2e8f696a2f834f2460a29b2c4376c2921f5713c4
Caio Silva <caiossantosp5560@gmail.com>
2024-05-23 15:27:48 -0300

    feat: add delete post route and functionality to the backend

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { create, getPosts } from '../controllers/post.controller.js';
+import { create, deletePosts, getPosts } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, create);
 router.get('/get-posts', getPosts);
+router.delete('/delete-post/:postId/:userId', verifyToken, deletePosts);
 
 export default router;
