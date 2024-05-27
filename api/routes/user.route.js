08adf5d0b83af0f8de7052de68f81edf7db3724d
Caio Silva <caiossantosp5560@gmail.com>
2024-05-27 15:47:14 -0300

    feat: craete a getUsers route and controller

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteUser, signout, test, updateUser } from '../controllers/user.controller.js';
+import { deleteUser, getUsers, signout, test, updateUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -11,6 +11,7 @@ router.get('/test', (req, res) => {
 router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
+router.get('/get-users', verifyToken, getUsers);
 
 export default router;
 
