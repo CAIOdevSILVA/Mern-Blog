a0394508596d6ed5ff56db50e37259896ecf044c
Caio Silva <caiossantosp5560@gmail.com>
2024-05-27 16:39:52 -0300

    fat: add a get users page and functionality in frontend

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,6 +1,6 @@
 import { Sidebar } from 'flowbite-react';
 import { useEffect, useState } from 'react';
-import { HiUser, HiArrowSmRight, HiDocumentText } from 'react-icons/hi';
+import { HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser } from 'react-icons/hi';
 import { Link, useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { signoutSuccess } from '../redux/user/userSlice';
@@ -54,16 +54,27 @@ const DashSidebar = () => {
             </Sidebar.Item>
           </Link>
 
-          {currentUser && (
-            <Link to={'/dashboard?tab=posts'}>
-              <Sidebar.Item
-                active={tab === 'posts'}
-                icon={HiDocumentText}
-                as='div'
-              >
-                Posts
-              </Sidebar.Item>
-            </Link>
+          {currentUser.isAdmin && (
+            <>
+              <Link to={'/dashboard?tab=posts'}>
+                <Sidebar.Item
+                  active={tab === 'posts'}
+                  icon={HiDocumentText}
+                  as='div'
+                  >
+                  Posts
+                </Sidebar.Item>
+              </Link>
+              <Link to={'/dashboard?tab=users'}>
+                <Sidebar.Item
+                  active={tab === 'users'}
+                  icon={HiOutlineUserGroup}
+                  as='div'
+                  >
+                  Users
+                </Sidebar.Item>
+              </Link>
+            </>
           )}
 
           <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer' onClick={handleSignOut}>
