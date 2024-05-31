30c54d05cc4e088b3a42727f367cf6ab4f9be851
Caio Silva <caiossantosp5560@gmail.com>
2024-05-31 15:38:10 -0300

    feat: add like comment route and functionality into backend

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { createComment, getPostComment } from '../controllers/comment.controller.js';
+import { createComment, getPostComment, likeComment } from '../controllers/comment.controller.js';
 
 const router = express.Router();
 
 router.post('/create-comment', verifyToken ,createComment);
 router.get('/get-post-comment/:postId', getPostComment);
+router.put('/like-comment/:commentId', verifyToken, likeComment);
 
 export default router;
