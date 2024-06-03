c04ef6ae46f8b3eeac09464c0d473efd2f8c6343
Caio Silva <caiossantosp5560@gmail.com>
2024-06-03 15:58:15 -0300

    feat: add edit comment functionality to the backend

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { createComment, getPostComment, likeComment } from '../controllers/comment.controller.js';
+import { createComment, editComment, getPostComment, likeComment } from '../controllers/comment.controller.js';
 
 const router = express.Router();
 
 router.post('/create-comment', verifyToken ,createComment);
 router.get('/get-post-comment/:postId', getPostComment);
 router.put('/like-comment/:commentId', verifyToken, likeComment);
+router.put('/edit-comment/:commentId', verifyToken, editComment);
 
 export default router;
