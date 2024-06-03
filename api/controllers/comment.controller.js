6d35b55b9f71a3330f19f69f233e07bca9d7f02a
Caio Silva <caiossantosp5560@gmail.com>
2024-06-03 14:59:06 -0300

    fix: error of sintaxe in comment.controller

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -38,10 +38,13 @@ export const likeComment = async(req, res, next) => {
     if(!comment) {
       return next(errorHandler(404, 'Comment not found'));
     }
-    const userIndex = comment.likes.indexOf(req.user.userId);
+
+    console.log(req.user.id)
+
+    const userIndex = comment.likes.indexOf(req.user.id);
     if(userIndex === -1) {
       comment.numberOfLikes += 1;
-      comment.likes.push(req.user.userId);
+      comment.likes.push(req.user.id);
     } else {
       comment.numberOfLikes -= 1;
       comment.likes.splice(userIndex, 1)
